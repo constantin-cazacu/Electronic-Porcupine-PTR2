d9ba63a22ea22edf88caf82cf7c34fe265d83bbd
Constantin Cazacu <71124836+constantin-cazacu@users.noreply.github.com>
2022-05-05 20:19:54 +0300

    Update router.ex

--- lib/router.ex
+++ lib/router.ex
@@ -8,20 +8,18 @@ defmodule Router do
 
   def receive_tweet(tweet) do
     id = UUID.uuid1()
-    GenServer.cast(__MODULE__, {:engagement, {id, tweet}})
-    GenServer.cast(__MODULE__, {:sentiment, {id, tweet}})
+    GenServer.cast(__MODULE__, {id, tweet})
   end
 
   def init(_opts) do
     {:ok, %{}}
   end
 
-  def handle_cast({:engagement, {id, tweet}}, state) do
+  def handle_cast({id, tweet}, state) do
     EngagementLoadBalancer.receive_tweet(id, tweet)
-  end
-
-  def handle_cast({:sentiment, {id, tweet}}, state) do
     SentimentLoadBalancer.receive_tweet(id, tweet)
+    RetweetLoadBalancer.receive_tweet(id, tweet)
+    {:noreply, state}
   end
 
 end
