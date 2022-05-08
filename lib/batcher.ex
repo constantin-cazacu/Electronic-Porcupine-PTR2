0b48395b870c5943af8908f0046ddaff4b38928e
Constantin Cazacu <71124836+constantin-cazacu@users.noreply.github.com>
2022-05-08 12:18:44 +0300

    Update batcher.ex

--- lib/batcher.ex
+++ lib/batcher.ex
@@ -2,19 +2,38 @@ defmodule Batcher do
   @moduledoc false
   use GenServer
 
-  def start_link(state, opts) do
-    GenServer.start_link(__MODULE__, state, opts)
+  def start_link() do
+    GenServer.start_link(__MODULE__, %{}, name: __MODULE__)
+  end
+
+  def add_record(record) do
+    GenServer.cast(__MODULE__, {:add, record})
+  end
+
+  def schedule_buffer() do
+    Process.send_after(self(), :free, 1000)
   end
 
   def init(_opts) do
-    {:ok, %{}}
+    schedule_batcher()
+    {:ok, %{batch: [], records_per_interval: 0}}
   end
 
-  def handle_call(_msg, _from, state) do
-    {:reply, :ok, state}
+  def handle_cast({:add, record}, state) do
+    record_buffer = [record | state.buffer]
+    case state.records_per_interval > 200 do
+      true ->
+        :open_connection_and_send
+        {:noreply, %{batch: [], records_per_interval: 0}}
+      false ->
+        {:noreply, state}
+    end
+    {:noreply, %{batch: record_buffer, records_per_interval: state.records_per_interval + 1}}
   end
 
-  def handle_cast(_msg, state) do
+  def handle_info(:free, state) do
+    :open_connection_and_send
+    schedule_buffer()
     {:noreply, state}
   end
 end
